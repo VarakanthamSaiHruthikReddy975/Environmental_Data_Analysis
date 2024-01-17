1cc29c823faec3e952e8dc0d1a38d216fa94c890
VarakanthamSaiHruthikReddy975 <105242080+VarakanthamSaiHruthikReddy975@users.noreply.github.com>
2024-01-17 16:53:22 -0600

    controller methods are added based on the service classes and yet to test both Air quality and Water quality end points

--- src/main/java/com/environment/environmentDataAnalysis/Controller/AirQualityController.java
+++ src/main/java/com/environment/environmentDataAnalysis/Controller/AirQualityController.java
@@ -6,10 +6,7 @@ import com.environment.environmentDataAnalysis.ServiceInterface.AirQualityServic
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,14 +27,14 @@ public class AirQualityController {
 
     //getting the air quality record by id
     @GetMapping("/{id}")
-    public ResponseEntity<AirQualityDTO> getAirQualityById(Long id){
+    public ResponseEntity<AirQualityDTO> getAirQualityById(@PathVariable Long id){
         AirQualityDTO airQualityDTO = airQualityService.getAirQualityById(id);
         return ResponseEntity.ok(airQualityDTO);
     }
 
     //adding an record into the air quality table
     @PostMapping("/addRecord")
-    public ResponseEntity<AirQualityDTO> addAirQualityRecord(AirQualityDTO airQualityDTO){
+    public ResponseEntity<AirQualityDTO> addAirQualityRecord(@RequestBody AirQualityDTO airQualityDTO){
         AirQualityDTO addAQRecord = airQualityService.addAirQualityRecord(airQualityDTO);
         return ResponseEntity.ok(addAQRecord);
     }
--- src/main/java/com/environment/environmentDataAnalysis/Controller/WaterQualityController.java
+++ src/main/java/com/environment/environmentDataAnalysis/Controller/WaterQualityController.java
@@ -1,7 +1,36 @@
 package com.environment.environmentDataAnalysis.Controller;
 
+import com.environment.environmentDataAnalysis.DTO.WaterQualityDTO;
+import com.environment.environmentDataAnalysis.Entity.WaterQuality;
+import com.environment.environmentDataAnalysis.ServiceInterface.WaterQualityService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@AllArgsConstructor
 public class WaterQualityController {
+    private final WaterQualityService waterQualityService;
+
+    public ResponseEntity<List<WaterQualityDTO>> getAllWaterQualityRecords(){
+
+        List<WaterQualityDTO> getAllRecords = (List<WaterQualityDTO>) waterQualityService.getAllRecords();
+        return ResponseEntity.ok(getAllRecords);
+    }
+
+    public ResponseEntity<WaterQualityDTO> getWaterQualityById(Long Id){
+        WaterQualityDTO waterQualityDTO = waterQualityService.getById(Id);
+        return ResponseEntity.ok(waterQualityDTO);
+    }
+
+    public ResponseEntity<WaterQualityDTO> addWaterQualityRecord(@RequestBody WaterQualityDTO waterQualityDTO)
+    {
+        WaterQualityDTO waterQualityDTO1 = waterQualityService.addAirQualityRecord(waterQualityDTO);
+        return ResponseEntity.ok(waterQualityDTO1);
+    }
+
 }

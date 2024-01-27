d1f48a0279539d263d5ca2a9ba9ddeac8a73c261
VarakanthamSaiHruthikReddy975 <105242080+VarakanthamSaiHruthikReddy975@users.noreply.github.com>
2024-01-26 18:33:22 -0600

    'The securityConfiguration.class' has been excluded for now in the main application class for testing end points

--- src/main/java/com/environment/environmentDataAnalysis/EnvironmentDataAnalysisApplication.java
+++ src/main/java/com/environment/environmentDataAnalysis/EnvironmentDataAnalysisApplication.java
@@ -2,8 +2,9 @@ package com.environment.environmentDataAnalysis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class EnvironmentDataAnalysisApplication {
 
 	public static void main(String[] args) {

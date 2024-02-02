5c96ded2113ae388f1e9d97b1071533443e225bc
VarakanthamSaiHruthikReddy975 <105242080+VarakanthamSaiHruthikReddy975@users.noreply.github.com>
2024-02-02 17:28:30 -0600

    Added the cross origin config file to connect to the fronend

--- src/main/java/com/environment/environmentDataAnalysis/Controller/AirQualityController.java
+++ src/main/java/com/environment/environmentDataAnalysis/Controller/AirQualityController.java
@@ -14,6 +14,7 @@ import java.util.List;
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/air-quality")
+@CrossOrigin(origins = "http://localhost:4200/AirQuality")
 public class AirQualityController {
     private final AirQualityService airQualityService;
 
--- src/main/java/com/environment/environmentDataAnalysis/EnvironmentDataAnalysisApplication.java
+++ src/main/java/com/environment/environmentDataAnalysis/EnvironmentDataAnalysisApplication.java
@@ -10,5 +10,4 @@ public class EnvironmentDataAnalysisApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EnvironmentDataAnalysisApplication.class, args);
 	}
-
 }
